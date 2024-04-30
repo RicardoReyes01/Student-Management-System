0899298c45343a9721a58d053ba7a518f4545750
Ricardo Reyes <reyesricardo1999dc@gmail.com>
2024-04-30 12:42:27 -0500

    Added input functionality to the add.html and views.py/ Created base.html to better navigate through pages

--- students/views.py
+++ students/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import render, redirect
 
 from .models import Student
-from .form import StudentForm
+from .forms import StudentForm
 
 # Create your views here.
 def index(request):
@@ -21,30 +22,9 @@ def add(request):
     if request.method == 'POST':
         form = StudentForm(request.POST)
         if form.is_valid():
-            new_student_number = form.cleaned_data['student_number']
-            new_first_name = form.cleaned_data['first_name']
-            new_last_name = form.cleaned_data['last_name']
-            new_email = form.cleaned_data['email']
-            new_major = form.cleaned_data['major']
-            new_gpa = form.cleaned_data['gpa']
-            
-            new_student = Student(
-                student_number = new_student_number,
-                first_name = new_first_name,
-                last_name = new_last_name,
-                email = new_email,
-                major = new_major,
-                gpa = new_gpa
-            )
-            
-            new_student.save()
-            
-            return render(request, 'students/add.html', {
-                'form': StudentForm(),
-                'success': True
-            })
-        else:
-            form = StudentForm()
-        return render(request, 'students/add.html', {
-            'form': StudentForm()
-        })
+            form.save()  # Save the form data to create a new student
+            return redirect('index')  # Redirect to the index page after successful form submission
+    else:
+        form = StudentForm()  # Create a new form instance if it's a GET request
+        
+    return render(request, 'students/add.html', {'form': form})
